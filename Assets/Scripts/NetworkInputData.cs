3072884effd50225642338be0fa6109acfc5a1d3
Mohammad Asim Khan <ak4514768@gmail.com>
2022-11-02 18:36:22 +0530

    First Commit

---
+++ Assets/Scripts/NetworkInputData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+public struct NetworkInputData : INetworkInput
+{
+    public Vector3 direction;
+}
